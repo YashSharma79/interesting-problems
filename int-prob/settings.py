56141b1ce91933234def178d1529fa14af5beab2
Yash Sharma <yashsharma2595@gmail.com>
2021-05-09 16:04:56 +0530

    Added IP

--- int-prob/settings.py
+++ int-prob/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'k#vbx_@^a&z+t+u6=u6i$ysfcj3z(+lw696&hx=t1$at-bn+09'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['59.65.155.53',
+ALLOWED_HOSTS = ['159.65.155.53',
                  'intprob.lastphoton.com',
                  '127.0.0.1']
 
